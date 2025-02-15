693e3e1b4ccb854804a8d4fafdee594ebc226e6a
Vadim-Strakhov <mars1597@gmail.com>
2025-02-15 18:26:46 +0300

    menu edits

--- src/components/18.LavaMenu/LavaMenu.tsx
+++ src/components/18.LavaMenu/LavaMenu.tsx
@@ -15,11 +15,14 @@ const LavaMenu = () => {
           type="radio"
           name="icons"
           id="m-home"
-          className={styles.home}
+          className={`${styles.home} ${styles.lavaInput}`}
           checked={selected === "m-home"}
           onChange={handleChange}
         />
-        <label htmlFor="m-home">
+        <label
+          htmlFor="m-home"
+          className={`${styles.lavaLabel} ${selected === "m-home" ? styles.checked : ""}`}
+        >
           <i className="fa-solid fa-home"></i>
           <span>Главная</span>
         </label>
@@ -27,11 +30,14 @@ const LavaMenu = () => {
           type="radio"
           name="icons"
           id="m-search"
-          className={styles.search}
+          className={`${styles.search} ${styles.lavaInput}`}
           checked={selected === "m-search"}
           onChange={handleChange}
         />
-        <label htmlFor="m-search">
+        <label
+          htmlFor="m-search"
+          className={`${styles.lavaLabel} ${selected === "m-search" ? styles.checked : ""}`}
+        >
           <i className="fa-solid fa-search"></i>
           <span>Поиск</span>
         </label>
@@ -39,11 +45,14 @@ const LavaMenu = () => {
           type="radio"
           name="icons"
           id="m-notification"
-          className={styles.notification}
+          className={`${styles.notification} ${styles.lavaInput}`}
           checked={selected === "m-notification"}
           onChange={handleChange}
         />
-        <label htmlFor="m-notification">
+        <label
+          htmlFor="m-notification"
+          className={`${styles.lavaLabel} ${selected === "m-notification" ? styles.checked : ""}`}
+        >
           <i className="fa-solid fa-bell"></i>
           <span>Уведомления</span>
         </label>
@@ -51,11 +60,14 @@ const LavaMenu = () => {
           type="radio"
           name="icons"
           id="m-favorites"
-          className={styles.favorites}
+          className={`${styles.favorites} ${styles.lavaInput}`}
           checked={selected === "m-favorites"}
           onChange={handleChange}
         />
-        <label htmlFor="m-favorites">
+        <label
+          htmlFor="m-favorites"
+          className={`${styles.lavaLabel} ${selected === "m-favorites" ? styles.checked : ""}`}
+        >
           <i className="fa-solid fa-heart"></i>
           <span>Избранное</span>
         </label>
@@ -63,11 +75,14 @@ const LavaMenu = () => {
           type="radio"
           name="icons"
           id="m-profile"
-          className={styles.profile}
+          className={`${styles.profile} ${styles.lavaInput}`}
           checked={selected === "m-profile"}
           onChange={handleChange}
         />
-        <label htmlFor="m-profile">
+        <label
+          htmlFor="m-profile"
+          className={`${styles.lavaLabel} ${selected === "m-profile" ? styles.checked : ""}`}
+        >
           <i className="fa-solid fa-user"></i>
           <span>Аккаунт</span>
         </label>
